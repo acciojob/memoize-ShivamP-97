ff1979e8ae127a9bf45ef4a932ca22df8d56ee3e
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-22 22:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function memoize(callback, resolver = JSON.stringify) {
-   
+  const cache = new Map();
+
+  function getKey(args) {
+    return resolver === JSON.stringify
+      ? JSON.stringify(args)
+      : resolver(...args);
   }
-  
-  module.exports = memoize;
-  
+
+  function memoized(...args) {
+    const key = getKey(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = callback(...args);
+    cache.set(key, result);
+    return result;
+  }
+
+  memoized.clear = function () {
+    cache.clear();
+  };
+
+  memoized.delete = function (...args) {
+    const key = getKey(args);
+    return cache.delete(key);
+  };
+
+  memoized.has = function (...args) {
+    const key = getKey(args);
+    return cache.has(key);
+  };
+
+  return memoized;
+}
+
+module.exports = memoize;
